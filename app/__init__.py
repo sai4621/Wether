dcd617650747a91e0664a4ba2b407d08ff1894a5
hynr <huzaifa478@gmail.com>
2024-05-14 01:15:58 -0400

    Origin

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,14 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_session import Session
+import os
 
-app = Flask(__name__, static_folder='static_app/dist')
+app = Flask(__name__, static_folder='../build', static_url_path='/')  # Adjust path if necessary
 app.config['ENV'] = 'development'
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'huzaifa_was_here'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # Replace with PostgreSQL URI for production
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SESSION_TYPE'] = 'filesystem'
 
@@ -21,3 +22,15 @@ from app import routes
 
 with app.app_context():
     db.create_all()
+
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/<path:path>')
+def static_proxy(path):
+    # Send any other route to the index.html
+    return send_from_directory(app.static_folder, path)
+
+if __name__ == '__main__':
+    app.run(debug=True)
